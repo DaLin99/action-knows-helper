acb814bf958378284f6edfb04f6d6e76caf2d7f8
linshaohui <linshaohui@kuaishou.com>
2021-02-20 18:54:15 +0800

    feat:失物招领&招聘信息&学院活动 列表初始化

---
+++ pages/recruitInfo/recruitInfo.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="recruit-info-container">
+    <div
+      v-for="(item, index) in recruitInfoList"
+      :key="index"
+      class="recruit-info-item"
+    >
+      <div class="container-left">
+        <div class="flex-box hover-height-light">
+          <span>{{ item.workType }}</span>
+          <span>{{ item.salary }}</span>
+        </div>
+        <div class="flex-box">
+          <span>{{ item.company }}</span>
+          <span>{{ item.palce }}</span>
+        </div>
+        <div class="flex-box">
+          <span
+            v-for="(
+              requirementItem, requirementIndex
+            ) in item.requirement"
+            :key="requirementIndex"
+          >
+            {{ requirementItem }}
+          </span>
+          <span class="date">{{ item.date }}</span>
+        </div>
+      </div>
+      <div class="container-right">
+        <img 
+          :src="item.imgPath" 
+          class="company-img" 
+          alt="" >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			recruitInfoList: [
+				{
+					company: '快手科技信息有限公司',
+					workType: '前端工程师-校招实习',
+					salary: '200/天',
+					palce: '北京',
+					requirement: ['在校应届', '本科'],
+					imgPath: '../../static/image/activity-bgc.png',
+					date: '2021-1-2',
+				},
+				{
+					company: '快手科技信息有限公司',
+					workType: '前端工程师-校招实习',
+					salary: '200/天',
+					palce: '北京',
+					requirement: ['在校应届', '本科'],
+					imgPath: '../../static/image/activity-bgc.png',
+					date: '2021-1-2',
+				},
+			],
+		};
+	},
+	methods: {},
+};
+</script>
+
+<style lang="less" scope>
+@import "./recruitInfo.less";
+</style>
---
+++ pages/schoolActivity/schoolActivity.vue
@@ -0,0 +1,77 @@
+<template>
+  <view class="activity-container">
+    <view
+      v-for="(item,index) in activityList"
+      :key="index"
+      class="acitivity-item-container">
+      <view class="activity-bgc-container">
+        <image
+          :src="item.activityBgcSrc"
+          class="activity-bgc"
+          mode=""/>
+      </view>
+      <text class="activitity-title">{{ item.activityTitle }}</text>
+      <text class="activitity-des">{{ item.activitityDes }}</text>
+      <view class="holder-date-container">
+        <view class="icon-and-hoder">
+          <uni-icons
+            class="icon"
+            type="contact"
+            size="20"/>
+          <text>{{ item.activityHolder }}</text>
+        </view>
+        <view class="date">
+          <text>{{ item.activityDate }}</text>
+        </view>
+      </view>
+    </view>
+
+  </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				activityList:[
+					{
+						activityBgcSrc:'../../static/image/activity-bgc.png',
+						activityTitle:'志愿者招募',
+						activityDate:'2020-9-1',
+						activityHolder:'组织名称',
+						activitityDes:'招聘要求：1.无关端正。2.担任干部的经历有限。3.幽默有趣4.写bug多的4.会做菜做得很好吃的'
+					},
+					{
+						activityBgcSrc:'../../static/image/activity-bgc.png',
+						activityTitle:'志愿者招募',
+						activityDate:'2020-9-1',
+						activityHolder:'组织名称',
+						activitityDes:'招聘要求：1.无关端正。2.担任干部的经历有限。3.幽默有趣4.写bug多的4.会做菜做得很好吃的'
+					},
+					{
+						activityBgcSrc:'../../static/image/activity-bgc.png',
+						activityTitle:'志愿者招募',
+						activityDate:'2020-9-1',
+						activityHolder:'组织名称',
+						activitityDes:'招聘要求：1.无关端正。2.担任干部的经历有限。吃的'
+					},
+					{
+						activityBgcSrc:'../../static/image/activity-bgc.png',
+						activityTitle:'志愿者招募',
+						activityDate:'2020-9-1',
+						activityHolder:'组织名称',
+						activitityDes:'招聘要求：1.无关端正。2.担任干部的经历有限。3.幽默有趣4.写bug多的4.会做菜做得很好吃的'
+					}
+				]
+			};
+		},
+		methods: {
+
+		}
+	};
+</script>
+
+
+<style lang="less" scope>
+@import "./schoolActivity.less";
+</style>
