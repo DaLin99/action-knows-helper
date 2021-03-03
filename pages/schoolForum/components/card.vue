aee5b3a04bc2bcacd7ef2aa755c6464c7d613ffe
xqbzheng <xqbzheng@tencent.com>
2021-03-04 01:46:01 +0800

    feat: 校园论坛初始化

---
+++ pages/schoolForum/components/card.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="forum-item">
+    <view class="content-top">
+      <title class="title mt10 mb5">{{ item.title }}</title>
+      <text class="response">{{ responseWords }}</text>
+    </view>
+    <text class="date">{{ item.date }}</text>
+  </view>
+</template>
+
+<script>
+  export default {
+    props:{
+      item: {
+        type: Object,
+        default:()=>({})
+      }
+    },
+    computed: {
+      responseWords() {
+        return this.item.latestResponse
+        ? `最新回复：${this.item.latestResponse}`
+        : '呀～暂无人回复'
+      }
+    }
+  };
+</script>
+
+<style lang="less" scoped>
+  .forum-item{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 160rpx;
+    border-bottom: 1rpx solid rgba(202, 199, 204, 0.5);
+    .title{
+      display: block;
+      font-size: 34rpx;
+      color: #000;
+    }
+    .response, .date{
+      color: #ccc;
+    }
+    .response {
+      font-size: 30rpx;
+    }
+    .date{
+      font-size: 26rpx;
+    }
+  }
+</style>
--- pages/schoolForum/forumList.vue
+++ pages/schoolForum/forumList.vue
@@ -1,26 +1,53 @@
 <template>
-	<view class="content">
-    <button open-type="getUserInfo">ss</button>
+	<view>
+    <view class="forum-list">
+      <card
+      class="mt20"
+      v-for="(item, index) in forumList"
+      :item="item"
+      :key="index" />
+    </view>
+    <!-- <button open-type="getUserInfo">ss</button>
 		<image class="logo" :src="avatarUrl"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 			<text class="title">{{userInfo}}</text>
-		</view>
+		</view>xw
 		<text class="user-info" v-for="key in Object.keys(userInfo)">
 			<text>
 				{{key}}: {{userInfo[key]}}
 			</text>
-		</text>
+		</text> -->
 	</view>
 </template>
 
 <script>
 	import {mapMutations} from 'vuex'
 	import {mapState} from 'vuex'
+  import card from './components/card.vue';
 	export default {
+    components:{card},
 		data() {
 			return {
 				title: 'Hello',
+        forumList: [
+          {
+            id: 1,
+            title: '昨天为啥停水了',
+            latestResponse: '听说爆水管了。。。。楼主不要慌',
+            date: '2020-01-01'
+          },{
+            id: 1,
+            title: '昨天为啥停水了',
+            latestResponse: '听说爆水管了。。。。楼主不要慌',
+            date: '2020-01-01'
+          },{
+            id: 1,
+            title: '昨天为啥停水了',
+            latestResponse: '',
+            date: '2020-01-01'
+          },
+        ]
 			}
 		},
 		onLoad() {
@@ -75,4 +102,7 @@
 		font-size: .5rem;
 		align-self: flex-start;
 	}
+  .forum-list{
+    padding: 0 50rpx;
+  }
 </style>
