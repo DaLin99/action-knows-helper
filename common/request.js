5e1286c5851bdce39c5a21dd9c5cd2738635424d
xqbzheng <xqbzheng@tencent.com>
2021-03-20 16:38:52 +0800

    user丢失

--- common/request.js
+++ common/request.js
@@ -11,35 +11,37 @@ import urlConfig from './config.js';
 
 const request = {};
 const headers = {};
-let openId;
-uni.getStorage({
-  key: 'userInfo',
-  success:(res)=>{
-    openId = res?.data?.openid;
-  }
-});
+
 request.globalRequest = (url, method, data, power) => {
-     return new Promise((resolve, reject) => {
-		 return uni.request({
-		         url: urlConfig + url,
-		         method,
-		         data: {
-               ...data,
-               userId: openId, // 固定把openid带上
-             },
-		         dataType: 'json',
-		         header: headers
-		     }).then(res => {
-		         if (res[1].data && res[1].data.code == 1) {
-		             resolve(res[1].data);
-		         } else {
-		             reject(res[1].data);
-		         }
-		     }).catch(parmas => {
-				console.warn(parmas);
-				reject('小程序后台发生位置异常');
-		 　　});
-	 });
+      let openId;
+      uni.getStorage({
+        key: 'userInfo',
+        success:(res)=>{
+          openId = res?.data?.openid;
+          return new Promise((resolve, reject) => {
+          		 return uni.request({
+          		         url: urlConfig + url,
+          		         method,
+          		         data: {
+                      ...data,
+                      userId: openId, // 固定把openid带上
+                    },
+          		         dataType: 'json',
+          		         header: headers
+          		     }).then(res => {
+          		         if (res[1].data && res[1].data.code == 1) {
+          		             resolve(res[1].data);
+          		         } else {
+          		             reject(res[1].data);
+          		         }
+          		     }).catch(parmas => {
+          				console.warn(parmas);
+          				reject('小程序后台发生位置异常');
+          		 　　});
+          });
+        }
+      });
+     
     
  };
 export default request;
