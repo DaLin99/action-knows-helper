9404a44bb45b6e3f0bc67cd61c3fd36464178e6d
xqbzheng <xqbzheng@tencent.com>
2021-03-21 15:59:27 +0800

    delete big file

--- pages/recruitInfo/recruitInfoList.vue
+++ pages/recruitInfo/recruitInfoList.vue
@@ -19,51 +19,51 @@
 </template>
 
 <script>
-import tabs from "../component/tabs/tabs.vue";
-import card from "./components/card.vue";
+import tabs from '../component/tabs/tabs.vue';
+import card from './components/card.vue';
 export default {
   components: { tabs, card },
   data() {
     return {
       recruitInfoList: [
         {
-          id: "1",
-          company: "快手科技信息有限公司",
-          workType: "前端工程师-校招实习",
-          salary: "200/天",
-          palce: "北京",
-          requirement: ["在校应届", "本科"],
-          imgPath: require("../../static/image/kuaishou.png"),
-          date: "2021-3-12",
+          id: '1',
+          company: '快手科技信息有限公司',
+          workType: '前端工程师-校招实习',
+          salary: '200/天',
+          palce: '北京',
+          requirement: ['在校应届', '本科'],
+          imgPath: 'https://xqbzheng-1300584219.cos.ap-guangzhou.myqcloud.com/image/kuaishou.png',
+          date: '2021-3-12',
         },
         {
-          id: "2",
-          company: "腾讯有限公司",
-          workType: "前端工程师-校招实习",
-          salary: "5000/月",
-          palce: "深圳",
-          requirement: ["在校应届", "本科", "Vue"],
-          imgPath: require("../../static/image/tengxun.png"),
-          date: "2021-3-2",
+          id: '2',
+          company: '腾讯有限公司',
+          workType: '前端工程师-校招实习',
+          salary: '5000/月',
+          palce: '深圳',
+          requirement: ['在校应届', '本科', 'Vue'],
+          imgPath: 'https://xqbzheng-1300584219.cos.ap-guangzhou.myqcloud.com/image/tengxun.png',
+          date: '2021-3-2',
         },
         {
-          id: "3",
-          company: "滴滴有限公司",
-          workType: "后端开发-校招实习",
-          salary: "4000/月",
-          palce: "杭州",
-          requirement: ["在校应届", "本科", "Java"],
-          imgPath: require("../../static/image/didi.jpeg"),
-          date: "2021-4-2",
+          id: '3',
+          company: '滴滴有限公司',
+          workType: '后端开发-校招实习',
+          salary: '4000/月',
+          palce: '杭州',
+          requirement: ['在校应届', '本科', 'Java'],
+          imgPath: 'https://xqbzheng-1300584219.cos.ap-guangzhou.myqcloud.com/image/didi.jpeg',
+          date: '2021-4-2',
         },
       ],
       activeTabIndex: 0,
       tabsList: [
         {
-          tabName: "最新招聘",
+          tabName: '最新招聘',
         },
         {
-          tabName: "我的收藏",
+          tabName: '我的收藏',
         },
       ],
     };
