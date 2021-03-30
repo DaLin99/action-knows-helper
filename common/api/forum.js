c23e506538be36f488ec408d680f6dbfaa64c3f8
xqbzheng <xqbzheng@tencent.com>
2021-03-31 02:00:07 +0800

    feat: 我的论坛

--- common/api/forum.js
+++ common/api/forum.js
@@ -1,25 +1,31 @@
-import request from "../request.js";
+import request from '../request.js';
 
-const MODULE = "schoolForum";
+const MODULE = 'schoolForum';
 
 // 获取校园论坛信息
 const fetchForumList = () => request.globalRequest(`${MODULE}/list`);
 // 发布帖子
 const submitForum = (params) =>
-  request.globalRequest(`${MODULE}/publish`, "post", params);
+  request.globalRequest(`${MODULE}/publish`, 'post', params);
 // 获取详情
 const fetchForumDetail = (params) =>
-  request.globalRequest(`${MODULE}/detail`, "get", params);
+  request.globalRequest(`${MODULE}/detail`, 'get', params);
 // 发布评论
 const submitComment = (params) =>
-  request.globalRequest(`${MODULE}/comment`, "post", params);
+  request.globalRequest(`${MODULE}/comment`, 'post', params);
 // 点赞
 const postThumbUp = (params) =>
-  request.globalRequest(`${MODULE}/thumbUp`, "post", params);
+  request.globalRequest(`${MODULE}/thumbUp`, 'post', params);
+
+// 删除
+const deleteForum = (params) =>
+  request.globalRequest(`${MODULE}/delete`, 'post', params);
+  
 export default {
   fetchForumList,
   submitForum,
   fetchForumDetail,
   submitComment,
   postThumbUp,
+  deleteForum,
 };
